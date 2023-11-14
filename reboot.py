bd5b5b0c75a5a31c194c236dc4132486c457b976
FH.Dev <felixpoletti5@gmail.com>
2023-11-14 22:59:34 +0100

    Update reboot.py

--- reboot.py
+++ reboot.py
@@ -1,8 +1,8 @@
 import os
 import time
 password = os.getenv("PASS")
-os.system("cd /home/vinted/vintedpy && git pull")
+os.popen("sudo -S %s"%("cd /home/vinted/vintedpy && git pull"), 'w').write(password)
 time.sleep(15)
-os.popen("sudo -S %s"%("pkill -9 main.py"), 'w').write(password)
+os.popen("sudo -S %s"%("pkill main"), 'w').write(password)
 time.sleep(15)
 os.popen("sudo -S %s"%("reboot"), 'w').write(password)
