af17988512eb1db4c81241fc2d21e1e695a2e026
FH.Dev <felixpoletti5@gmail.com>
2023-11-14 22:00:51 +0100

    Update reboot.py

--- reboot.py
+++ reboot.py
@@ -2,5 +2,5 @@ import os
 password = os.getenv("PASS")
 os.system("cd /home/vinted/vintedpy")
 os.system("git pull")
-os.popen("sudo pkill -9 python").write(password)
-os.popen("sudo reboot").write(password)
+os.popen("sudo -S %s"%("pkill -9 python"), 'w').write(password)
+os.popen("sudo -S %s"%("reboot"), 'w').write(password)
