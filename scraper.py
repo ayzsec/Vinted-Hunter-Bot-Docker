d8128be6e8b9373cdca542db790bd92c95841f5a
Ayz <xadadox@gmail.com>
2024-10-25 02:12:45 +0200

    Revert "Implemented scraping functionality to find new items in a Vinted search link, with logs showing successful item detection. Items are being inserted but not posted to the Discord channel; requested to resolve this issue."
    This reverts commit ac624a8d7b242fecd212a028f12114b381846491.

--- scraper.py
+++ scraper.py
@@ -71,14 +71,12 @@ def generate_embed(item: Any, sub_id: int, item_res: Any) -> hikari.Embed:
     Args:
         item (Any): Scraped item
         sub_id (int): Subscription ID
-        item_res (Any): Additional item details
 
     Returns:
         hikari.Embed: Generated embed
     """
-    try:
     if str(item["currency"]) == "EUR":
-        currency = "â‚¬"
+        currency = "€"
     else:
         currency = " " + str(item["currency"])
     embed = hikari.Embed()
@@ -86,17 +84,17 @@ def generate_embed(item: Any, sub_id: int, item_res: Any) -> hikari.Embed:
     embed.url = item["url"]
     embed.set_image(item["photo"]["url"])
     embed.color = hikari.Color(0x09B1BA)
-    embed.add_field("ðŸ’µ Prix","```" + str(item["price"]) + currency + " | " + str(item_res["item"]["total_item_price"]) + currency + " TTC ```", inline=True)
-    embed.add_field("âœ¨ Etat","```" + item_res["item"]["status"] + "```", inline=True)
-    embed.add_field("ðŸ«… Avis","```ðŸ‘" + str(item_res["item"]["user"]["positive_feedback_count"]) + " - ðŸ‘Ž" + str(item_res["item"]["user"]["negative_feedback_count"]) + "```", inline=True)
+    embed.add_field("💵 Prix","```" + str(item["price"]) + currency + " | " + str(item_res["item"]["total_item_price"]) + currency + " TTC ```", inline=True)
+    embed.add_field("✨ Etat","```" + item_res["item"]["status"] + "```", inline=True)
+    embed.add_field("🫅 Avis","```👍" + str(item_res["item"]["user"]["positive_feedback_count"]) + " - 👎" + str(item_res["item"]["user"]["negative_feedback_count"]) + "```", inline=True)
     embed.add_field(":label: Marque","```" + item_res["item"]["brand"] + "```", inline=True)
-    embed.add_field("ðŸ“ Taille","```" + item["size_title"] + "```", inline=True)
-    embed.add_field("ðŸ“ Loc","```" + item_res["item"]["user"]["city"] + " (" + item_res["item"]["user"]["country_title"] + ")" + "```", inline=True)
+    embed.add_field("📏 Taille","```" + item["size_title"] + "```", inline=True)
+    embed.add_field("📍 Loc","```" + item_res["item"]["user"]["city"] + " (" + item_res["item"]["user"]["country_title"] + ")" + "```", inline=True)
     
     date = datetime.fromtimestamp(
         int(item["photo"]["high_resolution"]["timestamp"])
     ).strftime("%d/%m/%Y, %H:%M:%S")
-    embed.set_footer(f'Published on {date} â€¢ Subscription #{str(sub_id)}')
+    embed.set_footer(f'Published on {date} • Subscription #{str(sub_id)}')
     embed.set_author(
         name="Posted by " + item["user"]["login"],
         url=item["user"]["profile_url"],
